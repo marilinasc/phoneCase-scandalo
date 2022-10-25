fa2f66f97944f524a28829762b766ad2c80a3b9b
marilinasc <marilina.scandalo@gmail.com>
2022-10-25 16:00:57 -0300

    categoria de productos

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,14 +6,27 @@ import Cart from './CartWidget'
 const Navbar = () => {
     return (
     <div className="Navbar">
-        <img src={reactLogo}></img>
-        <div className = "nav-items">
-            <a href=""> INICIO </a>
-            <a href=""> PRODUCTOS </a>
-            <a href=""> PERSONALIZÁ TU FUNDA </a>
-            <a href=""> LOCALES </a>
-            <a href=""> <Cart/> 0 </a>
-        </div>
+        <a href=""><img src={reactLogo}></img></a>
+        <ul className = "nav-items">
+            <li><a href=""> INICIO </a></li>
+            <li><a href=""> PRODUCTOS </a>
+                <ul>
+                    <li><a href=""> Deporte </a></li>
+                    <li><a href=""> Mandalas </a></li>
+                    <li><a href=""> Mundo animal </a></li>
+                    <li><a href=""> Personajes </a></li>
+                    <li><a href=""> Botanic </a></li>
+                    <li><a href=""> Astrologia </a></li>
+                    <li><a href=""> Lifestyle </a></li>
+                    <li><a href=""> Design </a></li>
+                    <li><a href=""> Fantasy </a></li>
+                    <li><a href=""> Ver todas </a></li>
+                </ul>
+            </li>
+            <li><a href=""> PERSONALIZÁ TU FUNDA </a></li>
+            <li><a href=""> LOCALES </a></li>
+            <li><a href=""> <Cart/> 0 </a></li>
+        </ul>
     </div>
     )
 }
